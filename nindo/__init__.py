325a55653a3fa802cdf00bd846bd9571367253bd
Merlintor <merlinfuchs2001@gmail.com>
2020-07-16 19:37:28 +0200

    setup project

---
+++ nindo/__init__.py
@@ -0,0 +1,5 @@
+__title__ = "nindo"
+__version__ = "0.0.1-pre"
+__author__ = "Merlin Fuchs"
+__license__ = "MIT"
+__copyright__ = "Copyright 2020 (c) Merlin Fuchs"
---
+++ setup.py
@@ -0,0 +1,43 @@
+import pathlib
+import re
+
+import setuptools
+
+ROOT = pathlib.Path(__file__).parent
+
+with open(ROOT / "README.md", "r") as f:
+    long_description = f.read()
+
+
+with open(ROOT / "discouple" / "__init__.py", encoding="utf-8") as f:
+    match = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE)
+    if match is None:
+        raise RuntimeError("Could not parse version.")
+    VERSION = match.group(1)
+
+with open(ROOT / "requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="discouple",
+    version=VERSION,
+    author="Merlin Fuchs",
+    author_email="contact@merlin.gg",
+    description="Unofficial api wrapper for nindo.de",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Merlintor/nindo.de",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=requirements,
+    python_requires=">=3.7",
+)
